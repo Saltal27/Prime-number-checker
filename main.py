7f6682804750ca90fd97209c3693e36453c78c2a
Sal Tal <110280707+Saltal27@users.noreply.github.com>
2022-10-03 21:01:38 +0000

    qiuck update

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 def prime_checker(number):
     prime_number = True
-    for num in range(1, number):
-        if num != 1 and number % num == 0:
+    for i in range(2, number):
+        if number % i == 0:
             prime_number = False
     if prime_number == True:
         print("It's a prime number.")
     else:
         print("It's not a prime number.")
-            
+
+
 n = int(input("Check this number: "))
 prime_checker(number=n)
-
-
